d9c67d40f2698e57fa4873e5a7607b5cd9247b6d
周旭 <zhouxusoft@gmail.com>
2023-11-15 03:24:00 +0800

    ..

--- Code/app.py
+++ Code/app.py
@@ -6,7 +6,7 @@ import random
 '''
 
 # 打开 options.json 文件并读取内容
-with open('options.json', 'r', encoding='utf-8') as f:
+with open('./conf/options.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 # 通过随机数，随机从列表中获得一个用户名
@@ -26,7 +26,7 @@ random_number = random.randint(0, len(data['user_info_icon_list']) - 1)
 user_info_icon = data['user_info_icon_list'][random_number]
 
 # 打开 config.json 文件并读取内容
-with open('config.json', 'r', encoding='utf-8') as f:
+with open('./conf/config.json', 'r', encoding='utf-8') as f:
     config_data = json.load(f)
 
 # 修改 config.json 字段 中的内容
@@ -38,6 +38,6 @@ config_data['user_info_icon'] = user_info_icon
 
 
 # 将修改后的内容写回 config.json 文件中
-with open('config.json', 'w', encoding='utf-8') as f:
+with open('./conf/config.json', 'w', encoding='utf-8') as f:
     # 写入文件时，默认是使用ASCII编码，因此中文字符会被转换成Unicode编码，故需要添加 ensure_ascii=False
     json.dump(config_data, f, ensure_ascii=False, indent=4)
--- Code/main.py
+++ Code/main.py
@@ -2,10 +2,50 @@ from bs4 import BeautifulSoup
 import json
 import re
 
-
 # 打开 options.json 文件并读取内容
-with open('config.json', 'r', encoding='utf-8') as f:
+with open('./conf/config.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
+
+# 读取并修改 index.html 文件
+def changeIndex():
+    # 打开 index.html 文件并读取内容
+    with open('index.html', 'r', encoding='utf-8') as file:
+        html_code = file.read()
+    # 修改顶部导航栏颜色
+    pattern = r"\.topcolor\s*{[^{}]*}"
+    html_code = re.sub(pattern, '.topcolor{ background-color: ' + data['top_nav_color'] + ';}', html_code)
+    # 修改左侧导航栏颜色
+    pattern = r"\.leftcolor\s*{[^{}]*}"
+    html_code = re.sub(pattern, '.leftcolor{ background-color: ' + data['left_nav_color'] + ';}', html_code)
+    # 修改 script 中的模块列表
+    pattern = r"modulelist\s*=\s*\[.*?\]"
+    html_code = re.sub(pattern, 'modulelist = ' + str(data['moduleList']), html_code)
+    # 修改 script 中的菜单按钮
+    pattern = r"menuicon\s*=\s*''"
+    html_code = re.sub(pattern, 'menuicon = \'' + data['menu_icon'] + '\'', html_code)
+    # 修改 script 中的用户图标
+    pattern = r"usericon\s*=\s*''"
+    html_code = re.sub(pattern, 'usericon = \'' + data['user_info_icon'] + '\'', html_code)
+    # 修改 script 中的顶部导航栏颜色
+    pattern = r"topnavcolor\s*=\s*''"
+    html_code = re.sub(pattern, 'topnavcolor = \'' + data['top_nav_color'] + '\'', html_code)
+
+    # print(html_code)
+
+    soup = BeautifulSoup(html_code, 'html.parser')
+    # 修改 index.html 中的标题
+    title_tag = soup.find('title')
+    title_tag.string = data['name'] # type: ignore
+    # 修改顶部导航栏中的软件名称
+    soup.find("div", class_="softnametitle").clear() # type: ignore
+    soup.find("div", class_="softnametitle").append(data['name']) # type: ignore
+    # 修改顶部导航栏右侧的用户名称
+    soup.find("div", class_="userinfoname").clear() # type: ignore
+    soup.find("div", class_="userinfoname").append(data['user_info_name']) # type: ignore
+
+    # 将修改后的内容写回 index.html 文件中
+    with open('index.html', 'w', encoding='utf-8') as file:
+        file.write(soup.prettify())
 
 # 读取并修改 mermaid.html 文件
 def changeMindmap():
@@ -20,24 +60,27 @@ def changeMindmap():
 
     soup = BeautifulSoup(html_code, 'html.parser')
 
-    soup.find("div", class_="soft_name").clear()
-    soup.find("div", class_="soft_name").append(data['name'])
-    soup.find("div", class_="purpose_text").clear()
-    soup.find("div", class_="purpose_text").append(data["purpose"])
-    soup.find("div", class_="target_text").clear()
-    soup.find("div", class_="target_text").append(data["target"])
-    soup.find("div", class_="main_text").clear()
-    soup.find("div", class_="main_text").append(data["main"])
-    soup.find("div", class_="tech_text").clear()
-    soup.find("div", class_="tech_text").append(data["tech"])
+    # 获取并修改软件相关信息
+    soup.find("div", class_="soft_name").clear() # type: ignore
+    soup.find("div", class_="soft_name").append(data['name']) # type: ignore
+
+    soup.find("div", class_="purpose_text").clear() # type: ignore
+    soup.find("div", class_="purpose_text").append(data["purpose"]) # type: ignore
+
+    soup.find("div", class_="target_text").clear() # type: ignore
+    soup.find("div", class_="target_text").append(data["target"]) # type: ignore
+
+    soup.find("div", class_="main_text").clear() # type: ignore
+    soup.find("div", class_="main_text").append(data["main"]) # type: ignore
+
+    soup.find("div", class_="tech_text").clear() # type: ignore
+    soup.find("div", class_="tech_text").append(data["tech"]) # type: ignore
 
     # print(soup)
     
     # 将修改后的内容写回 mindmap.html 文件中
     with open('mindmap.html', 'w', encoding='utf-8') as file:
-        file.write(soup.prettify())
-    
-    
+        file.write(soup.prettify()) 
 
 # 读取并修改 mermaid.html 文件
 def changeMermaid():
@@ -68,3 +111,5 @@ def changeMermaid():
         file.write(html_code)
 
 changeMindmap()
+changeMermaid()
+changeIndex()
